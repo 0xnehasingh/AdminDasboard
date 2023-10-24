7cfa3f3dadca018165ed3ba753d705457ff9bbdd
0xnehasingh.eth <67492324+0xnehasingh@users.noreply.github.com>
2023-10-25 00:47:55 +0530

    index js

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,36 +1,36 @@
-import { Button } from './Button';
-export  {  Button } ;
+import  Button  from './Button';
+export    {Button}  ;
 
-import { ThemeSettings } from './ThemeSettings';
+import ThemeSettings  from './ThemeSettings';
 export { ThemeSettings };
 
-import { Sidebar } from './Sidebar';
+import  Sidebar  from './Sidebar';
 export {  Sidebar } ;
 
-import {  Navbar } from './Navbar';
+import  Navbar  from './Navbar';
 export { Navbar };
 
-import { Footer } from './Footer';
+import  Footer from './Footer';
 export {  Footer };
 
 
-import {  Cart } from './Cart';
+import  Cart  from './Cart';
 export { Cart };
 
-import { default as Chat } from './Chat';
+import  Chat  from './Chat';
 export { Chat };
 
-import { Notification } from './Notification';
+import Notification  from './Notification';
 export { Notification };
 
 
-import { UserProfile } from './UserProfile';
-import { SparkLine } from './Charts/SparkLine';
-import {  LineChart } from './Charts/LineChart';
-import {  Stacked } from './Charts/Stacked';
-import {  Pie } from './Charts/Pie';
-import { ChartsHeader } from './ChartsHeader';
-import {  Header } from './Header';
+import  UserProfile  from './UserProfile';
+import  SparkLine  from './Charts/SparkLine';
+import   LineChart  from './Charts/LineChart';
+import   Stacked  from './Charts/Stacked';
+import  Pie  from './Charts/Pie';
+import  ChartsHeader  from './ChartsHeader';
+import  Header  from './Header';
 
 
 export { UserProfile } ;
